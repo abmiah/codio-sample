3d8a319245d7f3ef65c92647a76b663f9d83bd66
Amrol Miah <amrolmiah@gmail.com>
2023-08-14 23:15:55 +0000

    User input check that a name has been entered

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def func():
-    user_input = str(input("Hello, what is you're name: "))
+    user_input: str = ""
+
+    while True:
+        if len(user_input) == 0:
+            print("Please enter your name")
+        else:
+            user_input
+            break
+        user_input = str(input("Hello, what is you're name: "))   
+        
     print(f"Hello {user_input}, welcome to Coido") 
 
 func()
