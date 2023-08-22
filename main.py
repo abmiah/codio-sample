8b88fa2485c6387740f6e0f0be7645e024beb582
Amrol Miah <amrolmiah@gmail.com>
2023-08-22 21:37:15 +0000

    While loop now excepts age input

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 def func():
     user_input: str = ""
+    user_input_age: int = ""
 
     while True:
-        if len(user_input) == 0:
-            print("Please enter your name")
-        else:
-            user_input
-            break
-        user_input = str(input("Hello, what is you're name: "))   
-        
-    print(f"Hello {user_input}, welcome to Coido") 
+      if len(user_input) == 0:
+        try:
+          user_input = str(input("Hello, please enter your name > "))
+          continue
+        except ValueError:
+          print("Error")     
+      
+      if user_input:
+        try:
+          user_input_age = int(input(f"Hello, {user_input.title()} how old are you? "))
+          print(f"{user_input.title()} you are {user_input_age} years old, in dog years that would be {user_input_age + (15 + 9)*5}")
+          break
+        except:
+           print("Please enter a valid age") 
+    print("Welcome to MSc Cyber Security")
+      
 
 func()
